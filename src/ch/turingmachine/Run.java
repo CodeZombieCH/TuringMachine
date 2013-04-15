aabe71eb3040a993f7fb87811715076543552199
Sam <Sam@192.168.82.101>
2013-04-15 20:14:51 +0200

    add states

--- src/ch/turingmachine/Run.java
+++ src/ch/turingmachine/Run.java
@@ -1,11 +1,16 @@
 package ch.turingmachine;
 
 public class Run {
-	/**
-	 * @param args
-	 */
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Multiplication
+		State s1 = new State();
+		State s2 = new State();
+		
+		Transition t1 = new Transition("11B", "111", "NRR", s1);
+		Transition t2 = new Transition("1BB", "BBB", "RLN", s2);
+		Transition t3 = new Transition("11B", "111", "NLR", s2);
+		Transition t4 = new Transition("1BB", "BBB", "RRN", s1);
 	}
 }
