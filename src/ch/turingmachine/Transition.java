0a9bea3ab231590d9b8e9d699729ca145cfb4299
Marco Buess <buess@gmx.net>
2013-04-15 17:25:04 +0200

    getter und Setter definiert, anpassungen an Arrays

--- src/ch/turingmachine/Transition.java
+++ src/ch/turingmachine/Transition.java
@@ -2,16 +2,47 @@ package ch.turingmachine;
 
 public class Transition
 {
-
-	/**
-	 * @param args
-	 */
+	private char[] input;
+	private char[] output;
+	private char[] direction;
 	
 	public Transition(int inp, int outp, int dire) {
-	
-		char Input[] = new char[inp];
-		char Output[] = new char[outp];
-		char Direction[] = new char[dire];
-
+		setInput(new char[inp]);
+		setOutput(new char[outp]);
+		setDirection(new char[dire]);
+		
 	}
+
+	public char[] getOutput()
+	{
+		return output;
+	}
+
+	public void setOutput(char[] output)
+	{
+		this.output = output;
+	}
+
+	public char[] getInput()
+	{
+		return input;
+	}
+
+	public void setInput(char[] input)
+	{
+		this.input = input;
+	}
+
+	public char[] getDirection()
+	{
+		return direction;
+	}
+
+	public void setDirection(char[] direction)
+	{
+		this.direction = direction;
+	}
+
+
+	
 }
