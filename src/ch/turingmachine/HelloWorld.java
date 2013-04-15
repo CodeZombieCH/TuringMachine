05b01d046ef41061d893080cfaf0e8d9309a26ea
Marc-Andre Buehler <git@public.mabsoftware.ch>
2013-04-15 16:27:14 +0200

    Hello world

---
+++ src/ch/turingmachine/HelloWorld.java
@@ -0,0 +1,17 @@
+package ch.turingmachine;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
