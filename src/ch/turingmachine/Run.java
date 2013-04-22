a1c7f57d851e319943c4d70f794899b12619da5a
Sam <Sam@Sam-Slim>
2013-04-22 13:17:52 +0200

    Rename Class call

--- src/ch/turingmachine/Run.java
+++ src/ch/turingmachine/Run.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Run {
 
 	public static void main(String[] args) {
-		new FacultyMachine().run(new int[] {3});
+		new FactorialMachine().run(new int[] {3});
 		new MultiplicationMachine().run(new int[] {3, 5});
 	}
 }
