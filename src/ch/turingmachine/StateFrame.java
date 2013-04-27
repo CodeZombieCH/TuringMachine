6f8a2d4d152e8b72ed2d6800f004a3fbc68a85d8
Sam <Sam@192.168.0.111>
2013-04-27 12:41:47 +0200

    set image location

--- src/ch/turingmachine/StateFrame.java
+++ src/ch/turingmachine/StateFrame.java
@@ -1,17 +1,21 @@
 package ch.turingmachine;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class StateFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private DisplayImage panel;
+	private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	
 	public StateFrame(String state) {
 		panel = new DisplayImage(state);
-		
 		this.add(panel);
 		this.setSize(640, 240);
+		this.setLocation(dim.width/2 - getWidth()/2, (int) (dim.height*0.8 - getHeight()/2));
 	}
 	
 	public void setFrame(String state) {
